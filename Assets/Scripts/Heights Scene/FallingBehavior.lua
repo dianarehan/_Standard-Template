b125f930449ee8c3f159f63fdde4e710a54e641c
diana <daiana.rehan.dr@gmail.com>
2025-05-06 19:34:20 +0300

    Added some sfx in the scene + added haptic impulse to the left controller

--- Assets/Scripts/Heights Scene/FallingBehavior.lua
+++ Assets/Scripts/Heights Scene/FallingBehavior.lua
@@ -7,12 +7,15 @@ local coroutine = coroutine
 local isFalling = false
 local fallDistance = 20
 local fallDuration = 2
-
+local hapticComponent = nil
+local audioSource = nil
 
 function awake()
     if xrOrigin == nil then
         print("xrOrigin not assigned in Lua!")
     end
+    audioSource = xrOrigin:GetComponent(typeof(UnityEngine.AudioSource))
+    print("Audio source:", audioSource)
 
     if gameObject == nil then
         print("gameObject is nil!")
@@ -20,6 +23,10 @@ function awake()
         triggerObject = gameObject:GetComponent(typeof(UnityEngine.Collider))
         print("Trigger object:", triggerObject)
     end
+    if hapticPlayer == nil then
+        print("HapticImpulsePlayer not found on xrOrigin!")
+    end
+    hapticComponent = hapticPlayer:GetComponent(typeof(CS.UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics.HapticImpulsePlayer))
 end
 
 function on_trigger_enter(other)
@@ -28,6 +35,11 @@ function on_trigger_enter(other)
         elapsed = 0
         startPos = xrOrigin.transform.position
         endPos = startPos + Vector3.down * fallDistance
+
+        if hapticPlayer ~= nil then
+            local var = hapticComponent:SendHapticImpulse(0.5, 0.2) 
+            print("Haptic impulse sent!", var)
+        end
     end
 end
 
@@ -40,6 +52,12 @@ function update()
         else
             xrOrigin.transform.position = Vector3(0, 1, 0)
             isFalling = false
+            if hapticPlayer ~= nil then
+                hapticComponent:SendHapticImpulse(0.7, 0.3)
+            end
+            if fallingClip ~= nil then
+                audioSource:PlayOneShot(fallingClip)
+            end
         end
     end
 end
